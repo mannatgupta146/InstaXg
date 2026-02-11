4962ce2829e94726fee2079c0bb68ea96438cbce
mannatgupta146 <mannatgupta146@gmail.com>
2026-02-11 23:10:57 +0530

    await added

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 const connectToDb = async()=>{
-    mongoose.connect(`${process.env.MONGODB_URI}/instaxg`)
+    await mongoose.connect(`${process.env.MONGODB_URI}/instaxg`)
     .then(()=> {
         console.log('database connected');
     })
