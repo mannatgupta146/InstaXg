bd65a58edbc0eedee080af7eef7022a3c85f6050
mannatgupta146 <mannatgupta146@gmail.com>
2026-02-13 23:50:32 +0530

    fixed

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 const userModel = require('../models/user.model')
-const bycrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 const registerController = async(req, res) => {
@@ -7,36 +7,36 @@ const registerController = async(req, res) => {
 
     const isUserAlreadyExists = await userModel.findOne({
         $or: [
-            {username},
-            {email}
+            {email},
+            {username}
         ]
     })
 
     if(isUserAlreadyExists){
         return res.status(409).json({
-            message: "User already exists" + (isUserAlreadyExists.email == email ? 'User email already exists' : 'Username already exists')
+            message: "user already exists"
         })
     }
 
-    const hash = await bycrypt.hash(password, 10)
+    const hash = await bcrypt.hash(password, 10)
 
     const user = await userModel.create({name, username, email, password: hash, bio, profilePic})
 
     const token = jwt.sign(
-        {id: user._id},
+        {id: user.id},
         process.env.JWT_SECRET,
-        {expiresIn: '1d'}
+        {expiresIn: "1d"}
     )
 
-    res.cookie('jwt_token', token)
+    res.cookie("jwt_token", token)
 
     res.status(201).json({
-        message: "User registered successfully",
+        message: "user registered successfully",
         user: {
-            name: user.name,
-            username: user.username,
-            email: user.email,
-            bio: user.bio,
+            name: user.name, 
+            username: user.username, 
+            email: user.email, 
+            bio: user.bio, 
             profilePic: user.profilePic
         }
     })
@@ -54,33 +54,33 @@ const loginController = async(req, res) => {
 
     if(!user){
         return res.status(404).json({
-            message: "User not found"
+            message: "user not found"
         })
     }
 
-    const isPasswordMatched = bycrypt.compare(password, user.password)
+    const isPasswordMatched = await bcrypt.compare(password, user.password)
 
     if(!isPasswordMatched){
         return res.status(401).json({
-            message: "incoorect or invalid password"
+            message: "invalid password"
         })
     }
 
     const token = jwt.sign(
-        {id: user._id},
+        {id: user.id},
         process.env.JWT_SECRET,
-        {expiresIn: '1d'}
+        {expiresIn: "1d"}
     )
 
-    res.cookie('jwt_token', token)
+    res.cookie("jwt_token", token)
 
     res.status(200).json({
-        message: "User loggedin successfully",
+        message: "user loggedin successfully",
         user: {
-            name: user.name,
-            username: user.username,
-            email: user.email,
-            bio: user.bio,
+            name: user.name, 
+            username: user.username, 
+            email: user.email, 
+            bio: user.bio, 
             profilePic: user.profilePic
         }
     })
