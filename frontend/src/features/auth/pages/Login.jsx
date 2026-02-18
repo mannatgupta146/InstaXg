dd3b1dde43ef57dd529337504280ca070cb32bf4
mannatgupta146 <mannatgupta146@gmail.com>
2026-02-18 22:18:38 +0530

    login form done

--- frontend/src/features/auth/pages/Login.jsx
+++ frontend/src/features/auth/pages/Login.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { useState } from 'react'
+import '../style/form.scss'
 
 const Login = () => {
+
+    
+
   return (
-    <div>Login</div>
+    <main>
+        <form onSubmit={handleSubmit}>
+            <h1>Login</h1>
+
+            <input onChange={(e) => {setUsername(e.target.value)}}
+            type="text" 
+            placeholder='Enter username'
+            value = {username}
+            />
+            
+            <input onChange={(e) => {setPassword(e.target.value)}}
+            type="password" 
+            placeholder='Enter password' 
+            value = {password}
+            />
+
+            <button>Login</button>
+        </form>
+    </main>
   )
 }
 
--- frontend/src/features/auth/pages/Register.jsx
+++ frontend/src/features/auth/pages/Register.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React from "react"
 
 const Register = () => {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>Register</div>
+    <main>
+      <form onSubmit={handleSubmit}>
+        <h1>Login</h1>
+
+        <input
+          onChange={(e) => {
+            setUsername(e.target.value)
+          }}
+          type="text"
+          placeholder="Enter username"
+          value={username}
+        />
+
+        <input
+          onChange={(e) => {
+            setPassword(e.target.value)
+          }}
+          type="password"
+          placeholder="Enter password"
+          value={password}
+        />
+
+        <button>Login</button>
+      </form>
+    </main>
   )
 }
 
