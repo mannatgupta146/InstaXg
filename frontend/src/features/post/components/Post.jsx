f425f2eb21d06a00ad0304ae09e80327f502f06f
mannatgupta146 <mannatgupta146@gmail.com>
2026-02-26 00:50:27 +0530

    fixed post card

--- frontend/src/features/post/components/Post.jsx
+++ frontend/src/features/post/components/Post.jsx
@@ -1,40 +1,49 @@
-{posts.map((post) => (
-          <div key={post.id} className="post-card">
-            {/* HEADER */}
-            <div className="post-header">
-              <div className="user-info">
-                <div className="img-wrapper">
-                  <div className="inner-circle">
-                    <img src={user.profileImage} alt="user" />
-                  </div>
-                </div>
-                <span>{user.username}</span>
-              </div>
+import React from "react";
+import "../style/feed.scss"
 
-              <button className="unfollow-btn">Unfollow</button>
-            </div>
+const Post = ({ post }) => {
+  return (
+    <div className="post-card">
 
-            {/* POST IMAGE */}
-            <div className="post-image">
-              <img src={post.imgUrl} alt="post" />
-            </div>
-
-            {/* ACTIONS */}
-            <div className="post-actions">
-              <div className="left-icons">
-                <i className="fa-regular fa-heart"></i>
-                <i className="fa-regular fa-comment"></i>
-                <i className="fa-regular fa-paper-plane"></i>
-              </div>
-
-              <div className="right-icon">
-                <i className="fa-regular fa-bookmark"></i>
-              </div>
-            </div>
-
-            {/* CAPTION */}
-            <div className="post-caption">
-              <strong>{user.username}</strong> {post.caption}
+      {/* HEADER */}
+      <div className="post-header">
+        <div className="user-info">
+          <div className="img-wrapper">
+            <div className="inner-circle">
+              <img src={post.userProfilePic} alt="user" />
             </div>
           </div>
-        ))}
+          <span>{post.username}</span>
+        </div>
+
+        <button className="unfollow-btn">Unfollow</button>
+      </div>
+
+      {/* POST IMAGE */}
+      <div className="post-image">
+        <img src={post.imgUrl} alt="post" />
+      </div>
+
+      {/* ACTIONS */}
+      <div className="post-actions">
+        <div className="left-icons">
+          <i className="fa-regular fa-heart"></i>
+          <i className="fa-regular fa-comment"></i>
+          <i className="fa-regular fa-paper-plane"></i>
+        </div>
+
+        <div className="right-icon">
+          <i className="fa-regular fa-bookmark"></i>
+        </div>
+      </div>
+
+      {/* CAPTION */}
+      <div className="post-caption">
+        <strong>{post.username}</strong> {post.caption}
+      </div>
+
+    </div>
+  );
+};
+
+export default Post;
--- frontend/src/features/post/pages/Feed.jsx
+++ frontend/src/features/post/pages/Feed.jsx
@@ -1,25 +1,32 @@
-import React, { useEffect } from "react"
-import "../style/feed.scss"
-import { usePost } from "../hooks/usePost"
+import React, { useEffect } from "react";
+import "../style/feed.scss";
+import { usePost } from "../hooks/usePost";
+import Post from "../components/Post";
 
 const Feed = () => {
-
-  const {feed, handleGetFeed, loading} = usePost()
+  const { feed, handleGetFeed, loading } = usePost();
 
   useEffect(() => {
-    handleGetFeed()
-  },[])
+    handleGetFeed();
+  }, []);
 
-  if(loading || !feed) {
-    return <div>Loading...</div>
+  if (loading) {
+    return <div className="loading">Loading...</div>;
   }
+
+  if (!feed || feed.length === 0) {
+    return <div className="loading">No posts to show</div>;
+  }
+
   return (
     <main className="feed-container">
       <div className="feed">
-        
+        {feed.map((post) => (
+          <Post key={post._id} user={post.user} post={post} />
+        ))}
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
