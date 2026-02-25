af54c8cf5e75c3ddb0c4ef5a057665df2649026e
mannatgupta146 <mannatgupta146@gmail.com>
2026-02-25 16:39:01 +0530

    resgiter navigate to home after successful registration

--- frontend/src/features/auth/pages/Register.jsx
+++ frontend/src/features/auth/pages/Register.jsx
@@ -1,20 +1,31 @@
 import React from "react"
-import { useState } from 'react'
-import '../style/form.scss'
-import { Link } from "react-router-dom"
+import { useState } from "react"
+import "../style/form.scss"
+import { Link, useNavigate } from "react-router-dom"
 import { useAuth } from "../hooks/useAuth"
 
 const Register = () => {
   const [username, setUsername] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
- 
-  const { handleRegister } = useAuth()
 
-  const handleSubmit = async(e) => {
+  const { loading, handleRegister } = useAuth()
+
+  const navigate = useNavigate()
+
+  if (loading) {
+    return <h1>Loading...</h1>
+  }
+
+  const handleSubmit = async (e) => {
     e.preventDefault()
 
-   await handleRegister(username, email, password)
+    const res = await handleRegister(username, email, password)
+
+    console.log(res)
+    console.log(res.user)
+
+    navigate("/")
   }
 
   return (
@@ -51,7 +62,12 @@ const Register = () => {
 
         <button>Register</button>
 
-        <p>Already have an account? <Link className="toggleAuth" to='/login'>Login</Link></p>
+        <p>
+          Already have an account?{" "}
+          <Link className="toggleAuth" to="/login">
+            Login
+          </Link>
+        </p>
       </form>
     </main>
   )
